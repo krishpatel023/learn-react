dd5447c2ff019a0faa1c3badc05b46f45743b554
krishpatel023 <krishpatel0234@gmail.com>
2023-02-05 19:05:28 +0530

    States Example in Class Component

--- src/App.js
+++ src/App.js
@@ -1,22 +1,27 @@
 import './App.css';
-import React, { useState } from 'react';
+import React, {Component } from 'react';
 
+class App extends Component{
 
-function App() {
-  const [data,setData] = useState(0);
-  function updateData(){
-    return(
-      setData(data+1)
-    )
+  constructor(){
+    super();
+    this.state={
+      data:0
+    }
   }
-  return (
+  updateData(){
+    this.setState({data:this.state.data+1})
+  }  
+  render(){
+    return(
     <div className="App">
         <h1>Learn React</h1>
         <h2>Click Count</h2>
-        <h2>{data}</h2>
-        <button onClick={()=>updateData()}>Click Here</button>
-    </div>
-  );
+        <h2>{this.state.data}</h2>
+        <button onClick={()=>this.updateData()}>Click Here</button>
+    </div>      
+    )
+  }
 }
 
 export default App;
