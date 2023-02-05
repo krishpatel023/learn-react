b30faedbfe7ce57c50a3a0ae9f163d49d817b469
krishpatel023 <krishpatel0234@gmail.com>
2023-02-05 18:46:17 +0530

    State Change Example

--- src/App.js
+++ src/App.js
@@ -1,23 +1,20 @@
-import logo from './logo.svg';
 import './App.css';
+import React, { useState } from 'react';
+
 
 function App() {
+  const [data,setData] = useState(0);
+  function updateData(){
+    return(
+      setData(data+1)
+    )
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <h1>Learn React</h1>
+        <h2>Click Count</h2>
+        <h2>{data}</h2>
+        <button onClick={()=>updateData()}>Click Here</button>
     </div>
   );
 }
