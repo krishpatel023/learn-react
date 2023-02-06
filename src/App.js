41eff4c4e7f66eecd552ecc4f98fde30efb09c9a
krishpatel023 <krishpatel0234@gmail.com>
2023-02-06 16:00:01 +0530

    Getting Form Data

--- src/App.js
+++ src/App.js
@@ -2,18 +2,31 @@ import './App.css';
 import React, {useState} from 'react';
 
 function App(){
-  const[status,setStatus]=useState(true)
-  function changeStatus(){
-    status?
-    setStatus(false)
-    :setStatus(true)
+  const[name,setName]=useState("")
+  const[movie,setMovie]=useState("")
+  const[accepted,setAccepted]=useState(false)
+
+  function getFormData(e){
+    console.log(name,movie,accepted)
+    e.preventDefault()
   }
-  
   return(
     <div className="App">
-
-      <button onClick={()=>changeStatus()}>Toggle</button>
-      {status? <h1>Learn React</h1> : null}
+      <h1>Learn Forms</h1>
+      <form onSubmit={getFormData}>
+        <input type="text" placeholder="Full Name" onChange={(e)=>setName(e.target.value)}/> <br /><br />
+        <select onChange={(e)=>setMovie(e.target.value)}>
+          <option value="">Select Options</option>
+          <option value="Marvel">Marvel</option>
+          <option value="DC">DC</option>
+        </select>
+        <br /><br />
+        <input type="checkbox" name="accept" onChange={(e)=>setAccepted(e.target.checked)} />
+        <label htmlFor="accept">ACCEPT</label>
+        <br />
+        <button type="submit">Submit</button>
+      </form>
+      <h1>{name} {movie} {accepted}</h1>
     </div>
   )
 }
