4bb6003839f752515e2ff247f1cdc16ba5eaa23e
krishpatel023 <krishpatel0234@gmail.com>
2023-02-10 18:13:45 +0530

    componentDidMount Life Cycle Method

--- src/App.js
+++ src/App.js
@@ -1,17 +1,27 @@
 import './App.css';
-import React, {useState } from 'react';
-import Child from './Child'
+import React, {Component } from 'react';
 
-function App(){
-
-  const [name,setName] = useState("Krish");
-    return(
-      <div className='App'>
-        <h1>Learn React</h1>
-        <Child name={name}/>
-        <button onClick={()=>setName("Patel")}>Click Here</button>
-      </div>
-    )
+class App extends Component{
+  constructor(){
+      super();
+      console.log("constructor")
+      this.state={
+        name:"KRISH"
+      }
+  }
+  componentDidMount(){
+    console.log("componentDidMount")
+  }
+  render(){
+      console.log("render")
+      return(
+          <div className='App'>
+              <h1>Learn React</h1>
+              <button onClick={()=>{this.setState({name:"PATEL"})}}>CLICK</button>          
+              
+          </div>
+      )
+  }
 }
 
 export default App;
