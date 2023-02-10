ecaa927a99ab7aed0b6a553df6fb218781023d33
krishpatel023 <krishpatel0234@gmail.com>
2023-02-10 21:30:12 +0530

    ShouldComponentUpdate Life Cycle Method

--- README.md
+++ README.md
@@ -1,16 +1,7 @@
 NOTES:
 
-* Only works after any state/props updates.
-* If we have 4 states it will run on any of the updation of the state.
-* Always use CONDITION while using this or it will run INFINITELY.
-* It will not run on page load.
+* It can stop rendering.
+* Will only run when state and props are updated.
+* We can put a condition and if the condition is true than we can run some code like pausing render function.
+* Stoping some rendering results in improving speed of app.
 
-## ARGUMENTS 
-* preState --> It will print previous state. See in console. It will always show preState. 
-
-* preProps --> It will print previous props. See in console. It will always show preProps.
-
-* snapshot --> It will always be undefined here. If your component implements getSnapshotBeforeUpdate() lifecycle than only its value will be seen.
-
-
-## The main use of it is if we want compare current state and previous state.
--- src/App.js
+++ src/App.js
@@ -1,36 +1,25 @@
 import './App.css';
 import React, {Component} from 'react';
-import Child from './Child'
 
 class App extends Component{
   constructor(){
       super();
       console.log("constructor")
       this.state={
-        name:"KRISH",
         count:0
       }
   }
-  componentDidUpdate(preProps,preState,snapshot){
-    console.log("componentDidUpdate")
-    //It will print previous state.
-    //See in console. It will always show preState.
-    console.log("PRE-STATE",preState)
-    console.log("CURRENT-STATE",this.state)
-    //snapshot
-    //It will always be undefined here.
-    //If your component implements getSnapshotBeforeUpdate()
-    // lifecycle than only its value will be seen.
-    console.log(snapshot)
+  shouldComponentUpdate(){
+    console.log("shouldComponentUpdate")
+    //Need to write true by default if false it won't update the state
+    return true;
   }
   render(){
       console.log("render")
       return(
           <div className='App'>
               <h1>Learn React</h1>
-              <Child name={this.state.name}/>
-              <button onClick={()=>{this.setState({name:"PATEL"})}}>CLICK</button>   
-              <br />
+              <h2>{this.state.count}</h2>
               <button onClick={()=>{this.setState({count:this.state.count+1})}}>INCREASE COUNT</button>       
           </div>
       )
