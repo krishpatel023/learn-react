8965818a11651bf6c8058be7f0a1e04a534c7f75
krishpatel023 <krishpatel0234@gmail.com>
2023-02-11 23:34:59 +0530

    Fragments

--- README.md
+++ README.md
@@ -1,3 +1,8 @@
 NOTES:
 
+## Use fragment instead of div to wrap multiple elements and componets. It is more favourable.
+
+
+## Why it is used?
+It helps in handling multiple elements and multiple components and is used as a wrapper.
 
--- src/App.js
+++ src/App.js
@@ -1,21 +1,12 @@
+import { Fragment } from 'react';
 import './App.css';
-import Child from './Child'
 function App(){
-  //Nesting of array
-  const multi = [
-    {name:"Krish",email:"krish@gmail.com",contact:101},
-    {name:"Krish",email:"krish@gmail.com",contact:101},
-    {name:"Krish",email:"krish@gmail.com",contact:101},
-    {name:"Krish",email:"krish@gmail.com",contact:101}
-  ]
 
   return (
-    <div className="App">
+    <Fragment>
       <h1>Learn React</h1>
-      {multi.map((item,i) => (
-        <Child data={item}/>
-      ))}
-    </div>
+      <h1>Hello</h1>
+    </Fragment>
   );
 }
 
