19d9a0bf3df75c409f083d3d9e480238a24d7c20
krishpatel023 <krishpatel0234@gmail.com>
2023-02-11 15:37:32 +0530

    Bootstrap Installation

--- README.md
+++ README.md
@@ -1,3 +1,8 @@
 NOTES:
 
+Official Documentation:
+https://react-bootstrap.github.io/getting-started/introduction
+
+For Installation:
+npm install react-bootstrap bootstrap
 
--- src/App.js
+++ src/App.js
@@ -1,15 +1,12 @@
 import './App.css';
-//Adding the css file
-import './style.css'
-import style from './custom.module.css'
+import Button from 'react-bootstrap/Button';
 
 function App(){
   
   return(
     <div className='App'>
-      <h1 className='type1'>Type 1</h1>
-      <h1 style={{  backgroundColor: 'black', color: 'blue'}}>Type 2</h1>
-      <h1 className={style.success}>Type 3</h1>
+      <h1>Bootstarp</h1>
+      <Button>Click</Button>
     </div>
   )
 }
