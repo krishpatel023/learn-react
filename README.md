02ac8c606d2838290a46cdd734d77e9c7bfe4797
krishpatel023 <krishpatel0234@gmail.com>
2023-02-11 20:08:49 +0530

    Table implementation and Keys use in Map

--- README.md
+++ README.md
@@ -1,4 +1,3 @@
 NOTES:
 
-## Why do we prefer Map over ForLoop?
-The reason is that map works in return statement as well but for loop dosen't work in return statement.
+
--- src/App.js
+++ src/App.js
@@ -1,47 +1,36 @@
 import './App.css';
 
 function App(){
-  const students = ["Krish","Kashvi","Jay","Namra"]
-  //Use of objects
+
   const multi = [
     {name:"Krish",email:"krish@gmail.com",contact:0},
     {name:"Krish",email:"krish@gmail.com",contact:0},
     {name:"Krish",email:"krish@gmail.com",contact:0},
     {name:"Krish",email:"krish@gmail.com",contact:0}
   ]
-  //map looping
-  students.map((item)=>{
-    console.log(item)
-  })
-  //for loop
-  for(let i=0;i<students.length;i++){
-    console.log(students[i])
-  }
-  return(
-    <div className='App'>
-      <h1>Learn React</h1>
-      {
-        students.map((item)=>{
-        console.log(item)
-        })
-      }
-      {
-        multi.map((item)=>
-          <div style={{backgroundColor:"lightblue"}}>
-              <h1>Name:{item.name}</h1>
-              <h1>Email:{item.email}</h1>
-              <h1>Contact:{item.contact}</h1>
-          </div>
 
-        )
+  return (
+    <div className="App">
+      <h1>Learn React</h1>
+      <table border="1">
+      {//without tbody an error will be displayed
       }
-      {/* {
-        for(let i=0;i<students.length;i++){
-        console.log(students[i])
-        }
-      } */}
+      <tbody>
+        {multi.map((item,i) => (
+          <div>
+          {//Here key is not displayed and prevents an error.
+          }
+            <tr key={i}>
+              <hd>Name:{item.name}</hd>
+              <td>Email:{item.email}</td>
+              <td>Contact:{item.contact}</td>
+            </tr>
+          </div>
+        ))}
+        </tbody>
+      </table>
     </div>
-  )
+  );
 }
 
 export default App;
