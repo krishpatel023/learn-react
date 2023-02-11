6c661cc9d25586e5d94012eb1c89072fb8687f5e
krishpatel023 <krishpatel0234@gmail.com>
2023-02-11 21:05:13 +0530

    Nested Array

--- src/App.js
+++ src/App.js
@@ -1,34 +1,34 @@
 import './App.css';
 
 function App(){
-
+  //Nesting of array
   const multi = [
-    {name:"Krish",email:"krish@gmail.com",contact:0},
-    {name:"Krish",email:"krish@gmail.com",contact:0},
-    {name:"Krish",email:"krish@gmail.com",contact:0},
-    {name:"Krish",email:"krish@gmail.com",contact:0}
+    {name:"Krish",email:"krish@gmail.com",address:[{ House:101 , Area:"Nav"}]},
+    {name:"Krish",email:"krish@gmail.com",address:[{ House:101 , Area:"Nav"}]},
+    {name:"Krish",email:"krish@gmail.com",address:[{ House:101 , Area:"Nav"}]},
+    {name:"Krish",email:"krish@gmail.com",address:[{ House:101 , Area:"Nav"}]}
   ]
 
   return (
     <div className="App">
       <h1>Learn React</h1>
-      <table border="1">
-      {//without tbody an error will be displayed
-      }
-      <tbody>
-        {multi.map((item,i) => (
-          <div>
+      {multi.map((item,i) => (
+        <div key={i} style={{backgroundColor:"black",color:"green"}}>
           {//Here key is not displayed and prevents an error.
           }
-            <tr key={i}>
-              <hd>Name:{item.name}</hd>
-              <td>Email:{item.email}</td>
-              <td>Contact:{item.contact}</td>
-            </tr>
-          </div>
-        ))}
-        </tbody>
-      </table>
+          <h1>Name:{item.name}</h1>
+          <h1>Email:{item.email}</h1>
+          {
+            item.address.map((item1,i1)=>(
+              <div key={i1}>
+                <h1>House:{item1.House}</h1>
+                <h1>Area:{item1.Area}</h1>                
+              </div>
+
+            ))
+          }
+        </div>
+      ))}
     </div>
   );
 }
