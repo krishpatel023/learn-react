080b0231f6eac431f03f6e03d2330a715abfb2c2
krishpatel023 <krishpatel0234@gmail.com>
2023-02-12 16:43:21 +0530

    Uncontrolled Component

--- README.md
+++ README.md
@@ -1,5 +1,7 @@
 NOTES:
 
-## Controlled Component
+## Uncontrolled Component
 * When input fields are controlled with states then it is called as controlled component.
 * Uncontrolled component directly handled using DOM.
+* There should be some input field in uncontrolled component.
+* Can use both --> Ref & getElementById
--- src/App.js
+++ src/App.js
@@ -1,19 +1,26 @@
-import { useState } from 'react';
+import { useRef } from 'react';
 import './App.css';
 
 function App(){
-  const[val,setVal]=useState("")
-  const[item,setItem]=useState("")
+  let inputRef1 = useRef(null)
+  function handleSubmit(e){
+    e.preventDefault()
+    console.log(inputRef1.current.value)
+    //Both ways are true as main purpose is that input should not be handled by state.
+    let input2 = document.getElementById('input2').value
+    console.log(input2)
+  }
   return(
     <div className='App'>
       <h1>Learn React</h1>
-      <h2>Value : {val}</h2>
-      <input type="text" onChange={(event)=>setVal(event.target.value)} placeholder="Value"/>
+      <form onSubmit={(e)=>handleSubmit(e)}>
+      <input type="text" ref={inputRef1}/>
       <br />
-      <br />
-      <h2>Item: {item}</h2>
-      <input type="text" onChange={(event)=>setItem(event.target.value)} placeholder="Item"/>
 
+      <input id="input2" type="text"/>
+      <br />
+      <button>Submit</button>
+      </form>
     </div>
   )
 }
