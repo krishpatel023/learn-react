8cab49f9b12f1db43371c48871329d37ee364b18
krishpatel023 <krishpatel0234@gmail.com>
2023-02-12 15:58:50 +0530

    useRef

--- README.md
+++ README.md
@@ -1,8 +1,4 @@
 NOTES:
 
-## useMemo Hook
-* use Ref as less as possible.
-* Try not to manipulate DOM in React.JS.
-* Ref manipulates DOM.
-
-![ScreenShot](Screenshot_20230212_151415.png)
+## useRef
+* same as Ref.
--- src/App.js
+++ src/App.js
@@ -1,24 +1,19 @@
-import { Component, createRef } from 'react';
+import { useRef } from 'react';
 import './App.css';
 
-class App extends Component{
-  constructor(){
-    super();
-    this.inputRef = createRef();
+function App(){
+  let inputRef = useRef(null)
+  function handleRef(){
+    console.log(inputRef.current.value="1000")
   }
-  componentDidMount(){
-    console.log(this.inputRef)
-    //From here we manipulated DOM and the value will be 1000
-    console.log(this.inputRef.current.value="1000")
-  }
-  render(){
-    return(
-      <div className='App'>
-        <h1>Learn React</h1>
-        <input type="text" ref={this.inputRef} />
-      </div>
-    )
-  }
+  return(
+    <div className='App'>
+      <h1>Learn React</h1>
+      <input type="text" ref={inputRef} />
+      <br />
+      <button onClick={()=>handleRef()}>Click</button>
+    </div>
+  )
 }
 
 export default App;
