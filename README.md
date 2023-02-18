603a1e8f0d6e583f38e5262404fed89b828e1c53
krishpatel023 <krishpatel0234@gmail.com>
2023-02-18 10:10:12 +0530

    NavLink & Styling Navbar

--- README.md
+++ README.md
@@ -1,8 +1,4 @@
 NOTES:
 
-## Params
-For Params --> name is taken here as params and then transfered to user.js with the help of params and displayed there. This names is recieved from about page with link user/krish and user/kashvi
-
-App.js --> user/:name
-About.js --> user/krish & user/krish
-User.js --> Using params we get the data about the names and then we display it.
+## NavLink
+ We can't apply class to link so we use NavLink.
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,14 +1,17 @@
-import {Link} from 'react-router-dom';
+import {Link, NavLink} from 'react-router-dom';
 import React from 'react';
+import './css/NavBar.css'
 
 function NavBar(){
     return(
-        <div>
-            <Link to='/home'>Home</Link>
-            <br />
-            <Link to='/about'>About</Link>
-            <br />
-            <Link to='/user'>User</Link>
+        <div className='header'>
+            <ul className='navbar'>
+            {/* We can't apply class to link so we use NavLink. */}
+                <li><NavLink className='nav-bar-link' to='/home'>Home</NavLink></li>
+                <li><NavLink className='nav-bar-link' to='/about'>About</NavLink></li>
+                <li><NavLink className='nav-bar-link' to='/user'>User</NavLink></li>
+            </ul>
+
         </div>
     )
 }
